e3e49d6db4b0b51025da1410c1678e8fdb4fc759
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-25 17:46:33 +0100

    refact(login): implement right section, containing form.

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -3,16 +3,46 @@ import "./LoginPage.scss";
 const LoginPage = () => {
   return (
     <div className="login-container">
-      <div className="login-illustration">
-        <h1>Login illustration</h1>
+      <div className="left-section">
+        <div className="login-illustration">
+          <h1>Login illustration</h1>
+        </div>
       </div>
-      <div className="left-container">
+
+      <div className="right-section">
         <div className="login-form">
-          <h2>Welcome!</h2>
-          <p>Enter details to login.</p>
-          <button type="button" className="btn btn-primary">
-            Primary
-          </button>
+          <div className="login-form-text">
+            <h2>Welcome!</h2>
+            <p>Enter details to login.</p>
+          </div>
+
+          <form>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <h6>FORGOT PASSWORD?</h6>
+
+            <button type="submit" className="btn btn-primary w-100">
+              LOG IN
+            </button>
+          </form>
         </div>
       </div>
     </div>
