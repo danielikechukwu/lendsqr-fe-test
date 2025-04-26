12cc30032167006e359f02067a9761c93196e06a
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-26 21:07:34 +0100

    refact(routing): Add new paths.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -15,8 +15,29 @@ const AppRoutes = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/users/:id" element={<UserDetailPage />} />
+
+        <Route path="/guarators" element={<Dashboard />} />
+        <Route path="/loans" element={<UsersPage />} />
+        <Route path="/decision-models" element={<Dashboard />} />
+        <Route path="/savings" element={<UsersPage />} />
+        <Route path="/loan-request" element={<Dashboard />} />
+        <Route path="/whitelist" element={<UsersPage />} />
+        <Route path="/karma" element={<Dashboard />} />
+        <Route path="/organization" element={<UsersPage />} />
+        <Route path="/loan-products" element={<Dashboard />} />
+        <Route path="/savings-products" element={<UsersPage />} />
+        <Route path="/fees-and-charges" element={<Dashboard />} />
+        <Route path="/transactions" element={<UsersPage />} />
+
+        <Route path="/services" element={<UsersPage />} />
+        <Route path="/service-account" element={<Dashboard />} />
+        <Route path="/settlements" element={<UsersPage />} />
+        <Route path="/reports" element={<Dashboard />} />
+        <Route path="/preferences" element={<UsersPage />} />
+        <Route path="/fees-and-pricing" element={<Dashboard />} />
+        <Route path="/audit-logs" element={<UsersPage />} />
       </Route>
-      
+
     </Routes>
   );
 };
