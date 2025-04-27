199e432ff7d2984758d1a11778b122eca6f7f2cb
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-27 17:55:36 +0100

    refact(AppRoute): Any sidenav click should navigate to User component page only.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../features/auth/LoginPage";
 import MainLayout from "../layouts/MainLayout";
-import Dashboard from "../pages/Dashboard";
 import UsersPage from "../features/users/UsersPage";
 import UserDetailPage from "../features/users/UserDetailPage";
 
@@ -12,29 +11,29 @@ const AppRoutes = () => {
       <Route path="/" element={<LoginPage />} />
 
       <Route element={<MainLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<UsersPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/users/:id" element={<UserDetailPage />} />
 
-        <Route path="/guarators" element={<Dashboard />} />
+        <Route path="/guarators" element={<UsersPage />} />
         <Route path="/loans" element={<UsersPage />} />
-        <Route path="/decision-models" element={<Dashboard />} />
+        <Route path="/decision-models" element={<UsersPage />} />
         <Route path="/savings" element={<UsersPage />} />
-        <Route path="/loan-request" element={<Dashboard />} />
+        <Route path="/loan-request" element={<UsersPage />} />
         <Route path="/whitelist" element={<UsersPage />} />
-        <Route path="/karma" element={<Dashboard />} />
+        <Route path="/karma" element={<UsersPage />} />
         <Route path="/organization" element={<UsersPage />} />
-        <Route path="/loan-products" element={<Dashboard />} />
+        <Route path="/loan-products" element={<UsersPage />} />
         <Route path="/savings-products" element={<UsersPage />} />
-        <Route path="/fees-and-charges" element={<Dashboard />} />
+        <Route path="/fees-and-charges" element={<UsersPage />} />
         <Route path="/transactions" element={<UsersPage />} />
 
         <Route path="/services" element={<UsersPage />} />
-        <Route path="/service-account" element={<Dashboard />} />
+        <Route path="/service-account" element={<UsersPage />} />
         <Route path="/settlements" element={<UsersPage />} />
-        <Route path="/reports" element={<Dashboard />} />
+        <Route path="/reports" element={<UsersPage />} />
         <Route path="/preferences" element={<UsersPage />} />
-        <Route path="/fees-and-pricing" element={<Dashboard />} />
+        <Route path="/fees-and-pricing" element={<UsersPage />} />
         <Route path="/audit-logs" element={<UsersPage />} />
       </Route>
 
