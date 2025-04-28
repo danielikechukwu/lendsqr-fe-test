1bfd7d4f3c47230f9eb61ab23b31531ed3b6afbd
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-28 09:08:41 +0100

    refact(types): Implement typescript types for user that will be returned.

--- src/features/users/types.ts
+++ src/features/users/types.ts
@@ -3,3 +3,33 @@ export type UserCardProps = {
     iconSrc: string;
     numberOfUser: number;
 }
+
+export type User =     {
+    id: string;
+    organization: string;
+    username: string;
+    email: string;
+    phoneNumber: string;
+    dateJoined: string;
+    status: string;
+    fullName: string;
+    bvn: string;
+    gender: string;
+    maritalStatus: string;
+    children: string;
+    residenceType: string;
+    levelOfEducation: string;
+    employmentStatus: string;
+    sectorOfEmployment: string;
+    durationOfEmployment: string;
+    officeEmail: string;
+    monthlyIncome: string;
+    loanRepayment: string;
+    twitter: string;
+    facebook: string;
+    instagram: string;
+    guarantorName: string;
+    guarantorPhoneNumber: string;
+    guarantorEmail: string;
+    guarantorRelationship: string;
+  }
